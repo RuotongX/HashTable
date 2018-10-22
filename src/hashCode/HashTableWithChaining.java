b0b4f25d78689e2a777424d8b830fb32a5aabbd7
BakaX <bagadiyi@gmail.com>
2018-10-23 02:13:23 +1300

    This hashmap program finish

--- src/hashCode/HashTableWithChaining.java
+++ src/hashCode/HashTableWithChaining.java
@@ -1,62 +1,129 @@
 package hashCode;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+/**
+ * This class is used to create the hash table with chaining.
+ * @author RuotongXu 16935995
+ *
+ * @param <E>
+ */
 
 
 
 public class HashTableWithChaining<E> {
 	private double loadf = 0;
-	private int capacity = 0;
+	public int capacity = 0;
 	private int element = 0;
-	private LinkedList<E> []table;
-	
+	private ArrayList<E> []table;
+	/**
+	 * Initialize the default table size is 5, max load factor is 0.75 and make the table as a array of arraylist(the arraylist can store the collisions).
+	 */
 	public HashTableWithChaining() {
 		this.capacity = 5;
 		this.loadf = 0.75;
-		table = new LinkedList[this.capacity];
+		table = new ArrayList[this.capacity];
 	}
-	
+	/**
+	 * This is the add method which can get the input object then use convert to index to get the index which is the place in the table, when the 
+	 * number of items in this table achieve the capacity*load factor, it will call resize method to resize the table.
+	 * @param input
+	 */
 	public void add(E input) {
 		int code = convertToIndex(input.hashCode());
-		if(contains(input)) {
-			table[code] = new LinkedList<>();
+		if(table[code]!=null) {
 			table[code].add(input);
 		} else {
-			table[code] = new LinkedList<>();
+			table[code] = new ArrayList<>();
 			table[code].add(input);
 			if(++element > capacity*loadf) 
 				resizeTable();
 		}
-		
+		System.out.println(input.toString()+" has been added");
 	}
-	
+	/**
+	 * This is the remove method which first get the index of the input method then call the remove method in arraylist class, to remove that object.
+	 * @param input
+	 * @return
+	 */
 	public E remove(E input) {
-		
+		if(contains(input)) {
+			table[convertToIndex(input.hashCode())].remove(input);
+			--element;
+			return input;
+		} else {
+			return null;
+		}
 	}
+	/**
+	 * This is the contain method which first get the index of input method, if the arraylist is exist in this place, then it use contain method in arraylist to find that object.
+	 * @param input
+	 * @return
+	 */
 	public boolean contains(E input) {
 		int code = convertToIndex(input.hashCode());
-		if(table[code].isEmpty()) {
+		try {
+			if(table[code].isEmpty()) {
+				return false;
+			} else {
+				if(table[code].contains(input)) {
+					
+					return true;
+				} else {
+					return false;
+				}
+			}
+			
+		} catch(Exception e){
 			return false;
 		}
-		return false;
 	}
+	/**
+	 * This is the size method which get how many element in this table.(The collisions are not included)
+	 * @return
+	 */
 	public int size() {
 		return element;
 	}
+	/**
+	 * This is the tostring method of this class which display the table.
+	 */
 	public String toString() {
-		
+		String output = "";
+		for(int i =0;i<table.length;i++) {
+			if(table[i] == null||table[i].isEmpty()) {
+			} else {
+				output = output+"\nIndex: "+convertToIndex(table[i].get(0).hashCode());
+				for(E temp:table[i]) {
+					output = output+" "+temp.toString()+" |";
+				}
+			}
+		}
+		return output;
 	}
+	/**
+	 * This is the convertToIndex method which modulo the hashcode of the object and return the index number.
+	 * @param input
+	 * @return
+	 */
 	private int convertToIndex(int input) {
-		return input%capacity;
+		int temp = input%capacity;
+		if(temp<0) {
+			temp = -temp;
+		}
+		return temp;
 	}
+	/**
+	 * This is the method which make the table expand 2 times capacity. basically, it just copy the old table elements into the new table
+	 * and also get the new index number because the capacity number changes.
+	 */
 	private void resizeTable() {
-		capacity = capacity*2;
-		LinkedList<E>[] newTable = new LinkedList[capacity];
+		this.capacity = capacity*2;
+		ArrayList<E>[] newTable = new ArrayList[capacity];
 		for(int i =0;i<table.length;i++) {
 			if(table[i]!=null) {
+				int code = convertToIndex(table[i].get(0).hashCode());
+				newTable[code] = new ArrayList<>();
 				for(E e:table[i]) {
-					int code = convertToIndex(table[i].hashCode());
-					newTable[code] = new LinkedList<>();
 					newTable[code].add(e);
 				}
 				table[i].clear();
--- src/hashCode/Person.java
+++ src/hashCode/Person.java
@@ -1,20 +1,30 @@
 package hashCode;
-import java.util.Scanner;
 
+/**
+ * This is the person class which have the attributes for the person, it has the hashcode method.
+ * And also this class also include the main method.
+ * @author RuotongXu 16935995
+ *
+ */
 public class Person {
 	private String name;
-	private long phoneNum;
+	private String phoneNum;
 	private int age;
-	private int hashcode;
 	Person next;
-
-	public Person(String name,long phoneNum,int age) {
+/**
+ * This is the constructor of this class
+ * @param name
+ * @param phoneNum
+ * @param age
+ */
+	public Person(String name,String phoneNum,int age) {
 		this.name = name;
 		this.phoneNum = phoneNum;
 		this.age = age;
-		hashcode = this.hashcode;
 	}
-	
+	/**
+	 * This method is used to hash the code by the person name which convert each letter of person name into number.
+	 */
 	public int hashCode() {
 		int code = 0;
 		char[] na = this.name.toCharArray();
@@ -23,14 +33,71 @@ public class Person {
 		}
 		return code;
 	}
-	public int getHashcode() {
-		return hashcode;
+	/**
+	 * This is the toString method which return the attributes for person.
+	 */
+	public String toString() {
+		return " Name: "+this.name+" phone number: "+this.phoneNum+" age: "+age;
 	}
+	/**
+	 * This is the main method, which create many people first then add them to table then delete one, finally display the details for table.
+	 * @param args
+	 */
 	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please input your name");
-		String name = sc.nextLine();
-		Person p1 = new Person(name,0,0);
-		
+		HashTableWithChaining htwc = new HashTableWithChaining();
+		Person Kenis = new Person("Kenis","0264145554",35);
+		Person Monia = new Person("Monia","85148552",45);
+		Person Keniss = new Person("Kenis","02641455555544",96);
+		Person Rein = new Person("Rein","595614",74);
+		Person Widow = new Person("Widow","554788920",29);
+		Person Tracer = new Person("Tracery","0347893",25);
+		Person Ball = new Person("Ball","8996412",8);
+		Person Genji = new Person("Genji","6314789",38);
+		Person Mccree = new Person("Mccreemee","8851447",46);
+		Person Winston = new Person("Winston","9634766",12);
+		Person Mercy = new Person("Mercy","8852145",38);
+		Person Reaper = new Person("Reaper","8992144",48);
+		htwc.add(Kenis);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Monia);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Keniss);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Rein);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Widow);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Tracer);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Ball);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Genji);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Mccree);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Winston);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Mercy);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		htwc.add(Reaper);
+		System.out.println("The Person in this table is "+htwc.size()+" and the size of this table is "+htwc.capacity);
+		System.out.println();
+		System.out.println("Test for Ball is contains: "+htwc.contains(Ball));
+		System.out.println(htwc.remove(Ball)+" has been remove");
+		System.out.println("Test for Ball is contains: "+htwc.contains(Ball));
+		System.out.println();
+		System.out.println("The Hash Table");
+		System.out.println(htwc.toString());
 	}
 }
